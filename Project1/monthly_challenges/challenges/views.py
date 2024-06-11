43a81b986dac69ab3ba9d64fe71e42a2f283639c
Ammar Hany <ammarhanyezeldin@gmail.com>
2024-06-11 23:45:14 +0300

    templates and dynamic urls

--- Project1/monthly_challenges/challenges/views.py
+++ Project1/monthly_challenges/challenges/views.py
@@ -1,7 +1,12 @@
+# Import necessary modules from Django
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from urllib3 import request
 
 # Create your views here.
+
+# Define a dictionary containing monthly challenges
 monthly_challenges = {
     "january": "jan challenge",
     "february": "feb challenge",
@@ -14,27 +19,55 @@ monthly_challenges = {
     "september": "sept challenge",
     "october": "oct challenge",
     "november": "nov challenge",
-    "december": "dec challenge",
+    "december": None
 }
-def monthly_challenge(request,month):
 
+
+def index(request):
+    months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {"months": months})
+
+
+def monthly_challenge(request, month):
+    """
+    A view function to handle requests for monthly challenges by month name.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        month (str): The name of the month.
+
+    Returns:
+        HttpResponse: An HTTP response containing the monthly challenge text.
+        HttpResponseNotFound: A 404 Not Found response if the month is not supported.
+    """
     try:
         monthly_challenge_text = monthly_challenges[month]
-        return HttpResponse(monthly_challenge_text)
+        return render(request, "challenges/challenge.html", {
+            "text": monthly_challenge_text,
+            "month_name": month.capitalize()
+        })
     except:
         return HttpResponseNotFound("This month is not supported")
 
 
 def monthly_challenge_by_number(request, month):
+    """
+    A view function to handle requests for monthly challenges by month number.
 
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        month (int): The number of the month (1-based index).
+
+    Returns:
+        HttpResponseRedirect: A redirect response to the monthly challenge by month name.
+        HttpResponseNotFound: A 404 Not Found response if the month number is invalid.
+    """
     try:
         if month > len(monthly_challenges):
             return HttpResponseNotFound("This Month is invalid")
-        monthes = monthly_challenges.keys()
-        print(monthes[0])
-        retrun = HttpResponseRedirect("/challanges/"+monthes[month-1])
-
-        return HttpResponse("test")
+        months = list(monthly_challenges.keys())
+        redirect_month = months[month - 1]
+        redirect_path = reverse("monthly_challenge", args=[redirect_month])
+        return HttpResponseRedirect(redirect_path)
     except Exception as e:
-        print(e)
-        return HttpResponseNotFound("This month is not supported")
+        return HttpResponseNotFound("This month is not supported - Error: " + str(e))
