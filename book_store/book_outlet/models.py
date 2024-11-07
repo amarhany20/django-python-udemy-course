96c90ec5c3882094049b5042d145f47d9cd0d5de
Ammar Hany <ammarhanyezeldin@gmail.com>
2024-11-07 15:27:20 +0300

    Stopped

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
+import itertools
 
-# Create your models here.
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True ,max_length=100)
+    author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, default="", null=False,db_index=True)
+
+    def save(self, *args, **kwargs):
+        # Generate initial slug
+        self.slug = slugify(self.title)
+        # Ensure the slug is unique
+        for x in itertools.count(1):
+            if not Book.objects.filter(slug=self.slug).exists():
+                break
+            self.slug = f'{slugify(self.title)}-{x}'
+
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return f'{self.title} ({self.rating})'
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
+from django.db.models import Avg, Max
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Book
 # Create your views here.
+def index(request):
+    books = Book.objects.all().order_by('')
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
+    return render(request, 'book_outlet/index.html', {
+        'books': books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating,
+    })
+
+def book_detail(request, slug):
+
+    # try:
+    #     book = Book.objects.get(pk=book_id)
+    #     return render(request, 'book_outlet/book_detail.html', {
+    #         'title': book.title,
+    #         'author': book.author,
+    #         'rating':book.rating,
+    #         'is_bestselling': book.is_bestselling,
+    #     })
+    # except:
+    #     raise Http404('Book not found')
+
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating':book.rating,
+        'is_bestselling': book.is_bestselling,
+    })
+
